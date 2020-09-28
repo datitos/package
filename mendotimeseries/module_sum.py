fa2f7c0aeb168bf37458ebf0ba0ac7e9811babac
mvickm <mariavictoriamunafo@yahoo.com>
2020-09-28 15:51:13 -0300

    Add a new function

--- mendotimeseries/module_sum.py
+++ mendotimeseries/module_sum.py
@@ -11,3 +11,9 @@ def matmul(a, b):
     Multiplies two matrix
     '''
     return np.matmul(a, b).tolist()
+ 
+def substract(a,b):
+    '''
+    Calculates the difference between "a" and "b"
+    '''
+    return a - b
