a058f458a28ef68292553cbed61a2540a3543eb7
Leonardo Calderon <leogcalderon1994@gmail.com>
2020-09-29 11:26:27 -0300

    Add plot rw function

--- mendotimeseries/first_module.py
+++ mendotimeseries/first_module.py
@@ -27,3 +27,46 @@ def randomwalk(length):
     '''
     pasos=np.random.randint (-1,2,length)
     return pasos.cumsum()
+
+
+def plot_random_walks():
+    ''''
+    Simulate 12 random walks, plot them.
+    '''
+    N = 100000
+    random_walks = []
+
+    plt.figure(figsize = (15,7))
+
+    #Genera y grafica 12 random walks en el primer subplot
+    f = plt.subplot(2, 1, 1)
+    for i in range(12):
+        random_walks.append(randomwalk(N))
+        plt.plot(random_walks[i])
+        plt.title('12 caminatas al azar')
+
+    plt.xticks([])
+    #Recupero los limites del primer subplot
+    #asi los uso en los siguientes dos subplots
+    ymin, ymax = f.get_ylim()
+
+    #Busco los RW que mas y menos se alejan
+    dic = {i:np.sum(abs(rw)) for i,rw in enumerate(random_walks)}
+    menos_se_aleja = min(dic, key = dic.get)
+    mas_se_aleja = max(dic, key = dic.get)
+
+    #Plot del que menos se aleja
+    plt.subplot(2, 2, 3)
+    plt.plot(random_walks[menos_se_aleja])
+    plt.ylim([ymin, ymax ])
+    plt.xticks([])
+    plt.title('Caminata que menos se aleja')
+
+    #Plot del que mas se aleja
+    plt.subplot(2, 2, 4)
+    plt.plot(random_walks[mas_se_aleja])
+    plt.ylim([ymin, ymax ])
+    plt.xticks([])
+    plt.title('Caminata que mas se aleja')
+
+    plt.show()
